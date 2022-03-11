1bc6dcd9ad3210874dff7b8a3f11c0f9b93a5e61
shakhzod-star <77914936+shakhzod-star@users.noreply.github.com>
2022-03-11 17:30:54 +0500

    change carusel image

--- ui/src/views/Companiya.vue
+++ ui/src/views/Companiya.vue
@@ -22,8 +22,11 @@
             </div>
             <div class="text__info">
               <ul>
-                <li>Наша компетентность и стандарты качества гарантируют, что каждый фильтр FILTRON обеспечивает надежную защиту в течение всего периода рекомендуемого использования.</li>
-                <li>Тысячи механиков по всей Европе выбирают фильтры FILTRON и ценят решения, облегчающие их работу.</li>
+                <li>Наша компетентность и стандарты качества гарантируют, что каждый фильтр FILTRON обеспечивает
+                  надежную защиту в течение всего периода рекомендуемого использования.
+                </li>
+                <li>Тысячи механиков по всей Европе выбирают фильтры FILTRON и ценят решения, облегчающие их работу.
+                </li>
               </ul>
             </div>
           </div>
@@ -41,7 +44,9 @@
               СОВРЕМЕННЫЕ ПРОИЗВОДСТВЕННЫЕ ЛИНИИ
             </div>
             <div class="text__info">
-              Качество и надежность фильтров FILTRON - это заслуга современных и передовых технологий, используемых на заводах нашей компании. За производство фильтров бренда FILTRON отвечает свыше 2000 специалистов.            </div>
+              Качество и надежность фильтров FILTRON - это заслуга современных и передовых технологий, используемых на
+              заводах нашей компании. За производство фильтров бренда FILTRON отвечает свыше 2000 специалистов.
+            </div>
           </div>
         </div>
       </div>
@@ -50,7 +55,10 @@
       <div class="labaratori__left" data-aos="fade-left">
         <span></span>
         <div class="labaratori__left--title">КОНТРОЛЬ КАЧЕСТВА В ЛАБОРАТОРИИ</div>
-        <div class="text__info">Мы производим контроль как качества материалов, используемых для производства, так и прочности и эффективности готовых продуктов. В современной лаборатории нашей компании мы используем для этих целей свыше 60 методов исследования, которые соответствуют международным стандартам</div>
+        <div class="text__info">Мы производим контроль как качества материалов, используемых для производства, так и
+          прочности и эффективности готовых продуктов. В современной лаборатории нашей компании мы используем для этих
+          целей свыше 60 методов исследования, которые соответствуют международным стандартам
+        </div>
       </div>
       <div class="slide__filter">
         <div class="container">
@@ -89,42 +97,44 @@
           МЫ БЕРЕМ НА СЕБЯ ОТВЕТСТВЕННОСТЬ ЗА КАЧЕСТВО
         </div>
         <div class="text__info">
-          В случае обнаружения дефектов в продуктах марки FILTRON мы гарантируем не только замену продукта новым, но и покрытие трудозатрат у и замену поврежденных деталей. Точные условия гарантии описаны здесь.
+          В случае обнаружения дефектов в продуктах марки FILTRON мы гарантируем не только замену продукта новым, но и
+          покрытие трудозатрат у и замену поврежденных деталей. Точные условия гарантии описаны здесь.
         </div>
-        </div>
+      </div>
     </div>
     <labaratori/>
     <auto/>
   </div>
 </template>
 <script>
-  import { Splide, SplideSlide } from '@splidejs/vue-splide';
-  import { createSlides } from "../../utils/slides";
-  import '../assets/slide/splide.min.css'
-  import labaratori from '../components/FristPage/labaratori'
-  import auto from '../components/FristPage/autoscroll'
-  export default {
-    data() {
-      return {
-        options: {
-          rewind : true,
-          gap    : '5rem',
-          slides: createSlides(),
-          type   : 'loop',
-          perPage: 1,
-          autoplay: true,
-          drag   : 'free',
-        }
+import {Splide, SplideSlide} from '@splidejs/vue-splide';
+import {createSlides} from "../../utils/slides";
+import '../assets/slide/splide.min.css'
+import labaratori from '../components/FristPage/labaratori'
+import auto from '../components/FristPage/autoscroll'
+
+export default {
+  data() {
+    return {
+      options: {
+        rewind: true,
+        gap: '5rem',
+        slides: createSlides(),
+        type: 'loop',
+        perPage: 1,
+        autoplay: true,
+        drag: 'free',
       }
-    },
-    components: {
-      Splide,
-      SplideSlide,
-      labaratori,
-      auto
-    },
+    }
+  },
+  components: {
+    Splide,
+    SplideSlide,
+    labaratori,
+    auto
+  },
   mounted() {
-    window.addEventListener("scroll", function(){
+    window.addEventListener("scroll", function () {
       let animation = document.querySelector(".animation");
       animation.classList.toggle("sticky", window.scrollY > 500)
     })
@@ -132,21 +142,24 @@
 }
 </script>
 <style lang="scss" scoped>
-.all{
+.all {
   position: relative;
 }
-.animation{
+
+.animation {
   position: fixed;
   right: 100px;
   top: 745px;
   z-index: 1;
   display: none;
   cursor: pointer;
-  .img1{
+
+  .img1 {
     position: relative;
+
     .img2,
-    .img3{
-      img{
+    .img3 {
+      img {
         position: absolute;
         top: 20px;
         left: 0;
@@ -155,59 +168,75 @@
       }
     }
   }
-  &.sticky{
+
+  &.sticky {
     display: block;
   }
-  .img2{
-    img{
+
+  .img2 {
+    img {
       transition: all ease .4s;
     }
   }
-  &:hover{
-    .img2{
-      img{
+
+  &:hover {
+    .img2 {
+      img {
         top: -70px;
         transition: all ease .4s;
         opacity: 0;
       }
     }
-    .img3{
-      img{
-        animation:example 1.5s infinite ;
+
+    .img3 {
+      img {
+        animation: example 1.5s infinite;
         @keyframes example {
-          0%   {top: 10px}
-          50%  {top: 40px}
-          100% {top: 10px}
+          0% {
+            top: 10px
+          }
+          50% {
+            top: 40px
+          }
+          100% {
+            top: 10px
+          }
         }
       }
     }
   }
 }
-.companiya__main{
+
+.companiya__main {
   padding-top: 240px;
 }
-.filter{
+
+.filter {
   display: flex;
   align-items: center;
   justify-content: space-between;
-  .text{
+
+  .text {
     margin-right: 100px;
   }
-  .hr__span{
+
+  .hr__span {
     width: 250px;
     height: 4px;
     background: #e1e1e1;
     display: block;
     margin-bottom: 64px;
   }
-  .title__menu{
+
+  .title__menu {
     font-size: 36px;
     line-height: 43px;
     color: #282525;
     font-family: 'Marmelad-R';
     max-width: 867px;
   }
-  .text__info{
+
+  .text__info {
     font-size: 24px;
     line-height: 130%;
     color: #4F4F4F;
@@ -216,28 +245,33 @@
     margin-top: 32px;
   }
 }
-.filter2{
+
+.filter2 {
   display: flex;
   align-items: center;
   justify-content: space-between;
-  .text{
+
+  .text {
     margin-left: 100px;
   }
-  .hr__span{
+
+  .hr__span {
     width: 250px;
     height: 4px;
     background: #e1e1e1;
     display: block;
     margin-bottom: 64px;
   }
-  .title__menu{
+
+  .title__menu {
     font-size: 36px;
     line-height: 43px;
     color: #282525;
     font-family: 'Marmelad-R';
     max-width: 867px;
   }
-  .text__info{
+
+  .text__info {
     font-size: 24px;
     line-height: 130%;
     color: #4F4F4F;
@@ -246,45 +280,55 @@
     margin-top: 32px;
   }
 }
-.data__aos{
+
+.data__aos {
   transition-duration: 2s !important;
 }
-.title__componiya{
+
+.title__componiya {
   font-size: 48px;
   line-height: 57px;
   text-align: center;
   color: #282525;
 }
-.filimg1{
-  img{
+
+.filimg1 {
+  img {
     margin-right: 100px;
   }
 }
-.filimg2{
-  img{
+
+.filimg2 {
+  img {
     margin-left: 100px;
   }
 }
-.filter2{
+
+.filter2 {
   margin-top: 120px;
   margin-bottom: 64px;
 }
-li{
+
+li {
   list-style-type: disc !important;
-  &:first-child{
+
+  &:first-child {
     margin-bottom: 24px;
   }
 }
 </style>
 <style scoped lang="scss">
 @import "../assets/style/FirstPage/labaratori";
-[data-aos=fade-right]{
-  transform: translate3d(-1000px,0,0);
+
+[data-aos=fade-right] {
+  transform: translate3d(-1000px, 0, 0);
 }
-.labaratori__left{
+
+.labaratori__left {
   transition-duration: 3s !important;
 }
-.title__text{
+
+.title__text {
   font-size: 48px;
   line-height: 57px;
   color: #282525;
@@ -293,12 +337,15 @@ li{
   margin-top: 120px;
   margin-bottom: 92px;
 }
-.slide__filter{
+
+.slide__filter {
 
 }
-.splide__slide.is-visible{
+
+.splide__slide.is-visible {
   margin-right: 65px !important;
 }
+
 .my-slider-progress {
   background: #ccc;
 }
@@ -309,13 +356,15 @@ li{
   transition: width 400ms ease;
   width: 0;
 }
+
 //li{
 //  width: 406px !important;
 //}
-[data-aos=fade-right]{
-  transform: translate3d(1000px,0,0);
+[data-aos=fade-right] {
+  transform: translate3d(1000px, 0, 0);
 }
-[data-aos=fade-left]{
-  transform: translate3d(-1000px,0,0);
+
+[data-aos=fade-left] {
+  transform: translate3d(-1000px, 0, 0);
 }
 </style>
