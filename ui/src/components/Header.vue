4928f60e4310010e12bdec760186eeab888dbf9e
Ravshanjon Ismoilov <77914936+shakhzod-star@users.noreply.github.com>
2022-03-17 20:51:32 +0500

    filter header change

--- ui/src/components/Header.vue
+++ ui/src/components/Header.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="all">
     <header class="header">
+      <div class="active_menu"  :class=" isActive ? 'active' : ''  " @click="menu">
+      <span></span>
+      </div>
       <div class="logo">
         <a href="/">
           <img src="../assets/img/logo.svg" alt="">
@@ -8,65 +11,104 @@
       </div>
       <div class="menu">
         <ul>
-          <li>
+          <li v-for="menu in menus" :key="menu.index">
             <router-link
-                to="/"
+                :to="menu.path"
             >
-              Главная
-            </router-link>
-            <span></span>
-          </li>
-          <li>
-            <router-link
-                to="/product"
-            >
-              Продукты
-            </router-link>
-            <span></span>
-          </li>
-          <li>
-            <router-link
-                to="/kompaniya"
-            >
-              Компания
-            </router-link>
-            <span></span>
-          </li>
-          <li>
-            <router-link
-                to="/Sertificat"
-            >
-              Сертификаты
-            </router-link>
-            <span></span>
-          </li>
-          <li>
-            <router-link
-                to="/contact"
-            >
-              Контакты
+              {{ $t(menu.name) }}
             </router-link>
             <span></span>
           </li>
         </ul>
       </div>
-      <div class="search__language">
+      <div class="search__language" >
         <div class="search">
           <button>
             <img src="../assets/img/search.svg" alt="">
           </button>
         </div>
         <div class="language">
-          <select name="" id="">
-            <option value="RU">RU</option>
-            <option value="UZ">UZ</option>
-            <option value="EN">EN</option>
+          <select id="selectLanguage" @change="setLocale">
+            <option v-for="value in values" :value="value.lang" :key="value.index"> {{ value.lang }}</option>
           </select>
         </div>
       </div>
     </header>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      menus: [
+        {
+          path: '/',
+          name: 'main'
+        }, {
+          path: '/product',
+          name: 'Продукты'
+        }, {
+          path: 'kompaniya',
+          name: 'Компания'
+        }, {
+          path: '/Sertificat',
+          name: 'Сертификаты'
+        }, {
+          path: '/contact',
+          name: 'Контакты'
+        }
+      ],
+      values: [
+        {
+          lang: 'EN'
+        },
+        {
+          lang: 'RU'
+        },
+        {
+          lang: 'UZ'
+        }
+      ],
+      isActive: false
+
+    }
+  },
+  methods:{
+    setLocale(item) {
+      let locale = (item.path[0].value).toLowerCase()
+      import(`../languages/${locale}.json`).then((msg) => {
+        this.$i18n.setLocaleMessage(locale, msg);
+        this.$i18n.locale = locale;
+        console.log(locale)
+        localStorage.setItem("language", locale);
+      });
+    },
+    menu(){
+      this.isActive = !this.isActive
+    }
+
+  },
+  mounted() {
+    let selected = document.getElementById('selectLanguage')
+    selected.value = localStorage.getItem("language");
+    let name = localStorage.getItem("language");
+    if (name === undefined) {
+      name = "en"
+    }
+    if (selected.value === undefined) {
+      selected.value = "EN";
+    }
+    import(`../languages/${name}.json`).then((msg) => {
+      this.$i18n.setLocaleMessage(name, msg);
+      this.$i18n.locale = name;
+      selected.value = name.toUpperCase();
+      localStorage.setItem("language", name);
+    });
+  }
+}
+
+</script>
 <style scoped lang="scss">
 @import "../assets/style/Header";
 </style>
